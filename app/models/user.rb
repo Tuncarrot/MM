9a93884aaaac598a50277d58a3498b9d44177e97
Tuncarrot <adastunkiewicz@gmail.com>
2021-08-29 14:51:17 +0000

    :sparkles: add password creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
     validates :email, presence: true, length: {maximum: 100}, 
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
-
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_152809) do
+ActiveRecord::Schema.define(version: 2021_08_29_143628) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_152809) do
     t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
