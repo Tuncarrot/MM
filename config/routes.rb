bfc8f9009af64d1aed03897b3a5d591f6c53d100
Tuncarrot <adastunkiewicz@gmail.com>
2021-08-30 15:48:27 +0000

    :construction: add user signup form and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'users#new'
   get 'users/signup', to: 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
