ad98eb500a273171009e3c618ef6794592f2cf62
Tuncarrot <adastunkiewicz@gmail.com>
2021-09-28 20:09:33 +0000

    :bug: trying to fix login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     end
   end
   
-   private
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'users#new'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+resources :users
 end
