45d3908c05e80fd035f940424a17fbefbf8ee9ee
Tuncarrot <adastunkiewicz@gmail.com>
2021-11-09 01:22:17 +0000

    :sparkles: add users indexing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,12 @@ class SessionsController < ApplicationController
     #user = User.find_by(email: params[:session][:email].downcase) # OK, idk whats happening but... this is supposed to be the intended method, but it doesn't work
     user = User.find_by(params[:email])                            # This returns the user object
     if user && user.authenticate(params[:session][:password])      # Which in turn allows this to pass
-      p "Auth Success"
+      forwarding_url = session[:forwarding_url]
       reset_session
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      redirect_to user
+      redirect_to forwarding_url || user
     else
-      p params[:session][:password]
-      p "Auth Failed"
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
