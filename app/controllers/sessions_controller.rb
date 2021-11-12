d7e4fd6dae32c7d0093e2b4bc0ea675c10371faf
Tuncarrot <adastunkiewicz@gmail.com>
2021-11-12 01:54:16 +0000

    :features: add basic mailing functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,18 @@ class SessionsController < ApplicationController
     #user = User.find_by(email: params[:session][:email].downcase) # OK, idk whats happening but... this is supposed to be the intended method, but it doesn't work
     user = User.find_by(params[:email])                            # This returns the user object
     if user && user.authenticate(params[:session][:password])      # Which in turn allows this to pass
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to forwarding_url || user
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
-    before_save { self.email = email.downcase, self.username = username.downcase}
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
     validates :username, presence: true, length: {maximum: 50},
                     uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -39,6 +40,30 @@ class User < ApplicationRecord
     # We reuse the remember digest for convenience.
     def session_token
         remember_digest || remember
+    end
+
+    # Activates an account.
+    def activate
+        update_attribute(:activated,    true)
+        update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+
+private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 
 end
